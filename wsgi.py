430da6020717cdca0bfdbb037436cb783ce9a176
Chris Hasenpflug <chris@hasenpflug.net>
2013-06-26 23:27:54 -0500

    call bottle application differently

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,13 @@
 from sys import argv
 from os import environ, path
 
+from devpi_server.main import make_application
+
+
 datadir = path.join(environ['GONDOR_DATA_DIR'], '.devpi', 'server')
 secretfile = path.join(datadir, 'secretfile')
 
 argv.append('--datadir=%s' % datadir)
 argv.append('--secretfile=%s' % secretfile)
 
-from devpi_server.main import main
-
-application = main()
+application = make_application()
